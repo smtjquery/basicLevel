70fc72d7485b3248367f56ddf1c59616ff09e4cd
Łukasz Garboś <lukasz.garbos@gmail.com>
2015-07-26 20:33:25 +0200

    Make lint happy :)

--- app/scripts/service.js
+++ app/scripts/service.js
@@ -4,67 +4,67 @@
 
         function addNewTask(taskName) {
             var toDoTasksList = getStorageArray("toDoTasksList");
-            
+
             toDoTasksList.push(taskName);
-            
+
             setStorageArray("toDoTasksList", toDoTasksList);
         }
-        
+
         function moveTaskToProgres(taskName){
             var toDoTasksList = getStorageArray("toDoTasksList");
             var inProgressTasks = getStorageArray("inProgressTasks");
-            
-            var index = toDoTasksList.indexOf(taskName)
+
+            var index = toDoTasksList.indexOf(taskName);
             toDoTasksList.splice(index, 1);
-            
+
             inProgressTasks.push(taskName);
-            
+
             setStorageArray("toDoTasksList", toDoTasksList);
             setStorageArray("inProgressTasks", inProgressTasks);
         }
-        
+
         function moveTaskToDone(taskName){
             var doneTasks = getStorageArray("doneTasks");
             var inProgressTasks = getStorageArray("inProgressTasks");
-            
-            var index = inProgressTasks.indexOf(taskName)
+
+            var index = inProgressTasks.indexOf(taskName);
             inProgressTasks.splice(index, 1);
-            
+
             doneTasks.push(taskName);
-            
+
             setStorageArray("doneTasks", doneTasks);
             setStorageArray("inProgressTasks", inProgressTasks);
         }
-        
+
         function getAllDoneTasks(){
             return getStorageArray("doneTasks");
         }
-        
+
         function getAllInProgressTasks(){
             return getStorageArray("inProgressTasks");
         }
-        
+
         function getAllToDoTasks(){
             return getStorageArray("toDoTasksList");
         }
-        
+
         function getStorageArray(storageName){
             return JSON.parse(localStorage[storageName]);
-        }  
-        
+        }
+
         function setStorageArray(storageName, obj){
             localStorage[storageName] = JSON.stringify(obj);
         }
-    
+
         function init() {
             if(!localStorage.toDoTasksList){
                 localStorage.toDoTasksList =  JSON.stringify([]);
             }
-            
+
             if(!localStorage.inProgressTasks){
                 localStorage.inProgressTasks = JSON.stringify([]);
             }
-            
+
             if(!localStorage.doneTasks){
                 localStorage.doneTasks = JSON.stringify([]);
             }
